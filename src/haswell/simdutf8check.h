3e234e217745bb9f5c7d5cd62a2dbb974c3ae4ff
John Keiser <john@johnkeiser.com>
2019-10-20 17:30:30 -0700

    Genericize utf-8 check

--- src/haswell/simdutf8check.h
+++ src/haswell/simdutf8check.h
@@ -40,93 +40,72 @@ static inline __m256i push_last_2bytes_of_a_to_b(__m256i a, __m256i b) {
 }
 
 struct processed_utf_bytes {
-  __m256i raw_bytes;
-  __m256i high_nibbles;
-  __m256i carried_continuations;
+  simd_u8 raw_bytes;
+  simd_i8 high_nibbles;
+  simd_i8 carried_continuations;
 };
 
 struct utf8_checker {
-  __m256i has_error;
+  simd_u8 has_error;
   processed_utf_bytes previous;
 
-  utf8_checker() :
-      has_error{_mm256_setzero_si256()},
-      previous{_mm256_setzero_si256(), _mm256_setzero_si256(), _mm256_setzero_si256()} {}
-
-  really_inline void add_errors(__m256i errors) {
-    this->has_error = _mm256_or_si256(this->has_error, errors);
-  }
-
   // all byte values must be no larger than 0xF4
-  really_inline void check_smaller_than_0xF4(__m256i current_bytes) {
+  really_inline void check_smaller_than_0xF4(simd_u8 current_bytes) {
     // unsigned, saturates to 0 below max
-    this->add_errors( _mm256_subs_epu8(current_bytes, _mm256_set1_epi8(0xF4u)) );
+    this->has_error |= current_bytes.saturated_sub(0xF4u);
   }
 
-  really_inline __m256i continuation_lengths(__m256i high_nibbles) {
-    return _mm256_shuffle_epi8(
-        _mm256_setr_epi8(1, 1, 1, 1, 1, 1, 1, 1, // 0xxx (ASCII)
-                         0, 0, 0, 0,             // 10xx (continuation)
-                         2, 2,                   // 110x
-                         3,                      // 1110
-                         4, // 1111, next should be 0 (not checked here)
-                         1, 1, 1, 1, 1, 1, 1, 1, // 0xxx (ASCII)
-                         0, 0, 0, 0,             // 10xx (continuation)
-                         2, 2,                   // 110x
-                         3,                      // 1110
-                         4), // 1111, next should be 0 (not checked here)
-                         
-        high_nibbles);
+  really_inline simd_i8 continuation_lengths(simd_i8 high_nibbles) {
+    return high_nibbles.lookup4(
+      1, 1, 1, 1, 1, 1, 1, 1, // 0xxx (ASCII)
+      0, 0, 0, 0,             // 10xx (continuation)
+      2, 2,                   // 110x
+      3,                      // 1110
+      4);                     // 1111, next should be 0 (not checked here)
   }
 
-  really_inline __m256i carry_continuations(__m256i initial_lengths) {
-    __m256i right1 = _mm256_subs_epu8(
-        push_last_byte_of_a_to_b(this->previous.carried_continuations, initial_lengths),
-        _mm256_set1_epi8(1));
-    __m256i sum = _mm256_add_epi8(initial_lengths, right1);
+  really_inline simd_i8 carry_continuations(simd_i8 initial_lengths) {
+    simd_i8 prev_carried_continuations = initial_lengths.prev(this->previous.carried_continuations);
+    simd_i8 right1 = simd_i8(simd_u8(prev_carried_continuations).saturated_sub(1));
+    simd_i8 sum = initial_lengths + right1;
 
-    __m256i right2 = _mm256_subs_epu8(
-        push_last_2bytes_of_a_to_b(this->previous.carried_continuations, sum), _mm256_set1_epi8(2));
-    return _mm256_add_epi8(sum, right2);
+    simd_i8 prev2_carried_continuations = sum.prev2(this->previous.carried_continuations);
+    simd_i8 right2 = simd_i8(simd_u8(prev2_carried_continuations).saturated_sub(2));
+    return sum + right2;
   }
 
-  really_inline void check_continuations(__m256i initial_lengths, __m256i carries) {
+  really_inline void check_continuations(simd_i8 initial_lengths, simd_i8 carries) {
     // overlap || underlap
     // carry > length && length > 0 || !(carry > length) && !(length > 0)
     // (carries > length) == (lengths > 0)
     // (carries > current) == (current > 0)
-    __m256i overunder = _mm256_cmpeq_epi8(
-        _mm256_cmpgt_epi8(carries, initial_lengths),
-        _mm256_cmpgt_epi8(initial_lengths, _mm256_setzero_si256()));
-
-    this->add_errors( overunder );
+    this->has_error |= simd_u8(
+      (carries > initial_lengths) == (initial_lengths > simd_i8::zero()));
   }
 
   really_inline void check_carried_continuations() {
-    this->add_errors(
-      _mm256_cmpgt_epi8(this->previous.carried_continuations,
-      _mm256_setr_epi8(9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9,
-                       9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9,
-                       9, 9, 9, 9, 9, 9, 9, 1))
+    simd_i8 last_1(
+      9, 9, 9, 9, 9, 9, 9, 9,
+      9, 9, 9, 9, 9, 9, 9, 9,
+      9, 9, 9, 9, 9, 9, 9, 9,
+      9, 9, 9, 9, 9, 9, 9, 1
     );
+    this->has_error |= simd_u8(this->previous.carried_continuations > last_1);
   }
 
   // when 0xED is found, next byte must be no larger than 0x9F
   // when 0xF4 is found, next byte must be no larger than 0x8F
   // next byte must be continuation, ie sign bit is set, so signed < is ok
-  really_inline void check_first_continuation_max(__m256i current_bytes,
-                                                      __m256i off1_current_bytes) {
-    __m256i maskED =
-        _mm256_cmpeq_epi8(off1_current_bytes, _mm256_set1_epi8(0xEDu));
-    __m256i maskF4 =
-        _mm256_cmpeq_epi8(off1_current_bytes, _mm256_set1_epi8(0xF4u));
-
-    __m256i badfollowED = _mm256_and_si256(
-        _mm256_cmpgt_epi8(current_bytes, _mm256_set1_epi8(0x9Fu)), maskED);
-    __m256i badfollowF4 = _mm256_and_si256(
-        _mm256_cmpgt_epi8(current_bytes, _mm256_set1_epi8(0x8Fu)), maskF4);
-
-    this->add_errors( _mm256_or_si256(badfollowED, badfollowF4) );
+  really_inline void check_first_continuation_max(simd_u8 current_bytes,
+                                                  simd_u8 off1_current_bytes) {
+    simd_m8 prev_ED = off1_current_bytes == 0xEDu;
+    simd_m8 prev_F4 = off1_current_bytes == 0xF4u;
+    // Check if ED is followed by A0 or greater
+    simd_m8 ED_too_large = (simd_i8(current_bytes) > simd_i8::splat(0x9Fu)) & prev_ED;
+    // Check if F4 is followed by 90 or greater
+    simd_m8 F4_too_large = (simd_i8(current_bytes) > simd_i8::splat(0x8Fu)) & prev_F4;
+    // These will also error if ED or F4 is followed by ASCII, but that's an error anyway
+    this->has_error |= simd_u8(ED_too_large | F4_too_large);
   }
 
   // map off1_hibits => error condition
@@ -136,82 +115,73 @@ struct utf8_checker {
   // F       => < F1 && < 90
   // else      false && false
   really_inline void check_overlong(__m256i current_bytes,
-                                        __m256i off1_current_bytes,
-                                        __m256i high_nibbles) {
-    __m256i off1_high_nibbles = push_last_byte_of_a_to_b(this->previous.high_nibbles, high_nibbles);
-    __m256i initial_mins = _mm256_shuffle_epi8(
-        _mm256_setr_epi8(-128, -128, -128, -128, -128, -128, -128, -128, -128,
-                         -128, -128, -128, // 10xx => false
-                         0xC2u, -128,      // 110x
-                         0xE1u,            // 1110
-                         0xF1u,            // 1111
-                         -128, -128, -128, -128, -128, -128, -128, -128, -128,
-                         -128, -128, -128, // 10xx => false
-                         0xC2u, -128,      // 110x
-                         0xE1u,            // 1110
-                         0xF1u),           // 1111
-        off1_high_nibbles);
+                                    __m256i off1_current_bytes,
+                                    simd_i8 high_nibbles) {
+    simd_i8 off1_high_nibbles = high_nibbles.prev(this->previous.high_nibbles);
 
-    __m256i initial_under = _mm256_cmpgt_epi8(initial_mins, off1_current_bytes);
+    // Two-byte characters must start with at least C2
+    // Three-byte characters must start with at least E1
+    // Four-byte characters must start with at least F1
+    simd_i8 initial_mins = off1_high_nibbles.lookup4(
+      -128, -128, -128, -128, -128, -128, -128, -128, // 0xxx -> false
+      -128, -128, -128, -128,                         // 10xx -> false
+      0xC2u, -128,                                    // 1100 -> C2
+      0xE1u,                                          // 1110
+      0xF1u                                           // 1111
+    );
+    simd_m8 initial_under = initial_mins > simd_i8(off1_current_bytes);
 
-    __m256i second_mins = _mm256_shuffle_epi8(
-        _mm256_setr_epi8(-128, -128, -128, -128, -128, -128, -128, -128, -128,
-                         -128, -128, -128, // 10xx => false
-                         127, 127,         // 110x => true
-                         0xA0u,            // 1110
-                         0x90u,            // 1111
-                         -128, -128, -128, -128, -128, -128, -128, -128, -128,
-                         -128, -128, -128, // 10xx => false
-                         127, 127,         // 110x => true
-                         0xA0u,            // 1110
-                         0x90u),           // 1111
-        off1_high_nibbles);
-    __m256i second_under = _mm256_cmpgt_epi8(second_mins, current_bytes);
-    this->add_errors( _mm256_and_si256(initial_under, second_under) );
+    // Two-byte characters starting with at least C2 are always OK
+    // Three-byte characters starting with at least E1 must be followed by at least A0
+    // Four-byte characters starting with at least F1 must be followed by at least 90
+    simd_i8 second_mins = off1_high_nibbles.lookup4(
+      -128, -128, -128, -128, -128, -128, -128, -128, -128, // 0xxx => false
+      -128, -128, -128,                                     // 10xx => false
+      127, 127,                                             // 110x => true
+      0xA0u,                                                // 1110
+      0x90u                                                 // 1111
+    );
+    simd_m8 second_under = second_mins > simd_i8(current_bytes);
+    this->has_error |= simd_u8(initial_under & second_under);
   }
 
-  really_inline void count_nibbles(__m256i bytes, struct processed_utf_bytes *answer) {
+  really_inline void count_nibbles(simd_u8 bytes, struct processed_utf_bytes *answer) {
     answer->raw_bytes = bytes;
-    answer->high_nibbles = _mm256_and_si256(_mm256_srli_epi16(bytes, 4), _mm256_set1_epi8(0x0F));
+    answer->high_nibbles = simd_i8((bytes >> 4) & 0x0F);
   }
 
   // check whether the current bytes are valid UTF-8
   // at the end of the function, previous gets updated
-  really_inline void check_utf8_bytes(__m256i current_bytes) {
+  really_inline void check_utf8_bytes(simd_u8 current_bytes) {
     struct processed_utf_bytes pb {};
     this->count_nibbles(current_bytes, &pb);
 
     this->check_smaller_than_0xF4(current_bytes);
 
-    __m256i initial_lengths = this->continuation_lengths(pb.high_nibbles);
+    simd_i8 initial_lengths = this->continuation_lengths(pb.high_nibbles);
 
     pb.carried_continuations = this->carry_continuations(initial_lengths);
 
     this->check_continuations(initial_lengths, pb.carried_continuations);
 
-    __m256i off1_current_bytes =
-        push_last_byte_of_a_to_b(this->previous.raw_bytes, pb.raw_bytes);
+    simd_u8 off1_current_bytes = pb.raw_bytes.prev(this->previous.raw_bytes);
     this->check_first_continuation_max(current_bytes, off1_current_bytes);
 
     this->check_overlong(current_bytes, off1_current_bytes, pb.high_nibbles);
     this->previous = pb;
   }
 
-  really_inline void check_next_input(__m256i in) {
-    __m256i high_bit = _mm256_set1_epi8(0x80u);
-    if (likely(_mm256_testz_si256(in, high_bit) == 1)) {
+  really_inline void check_next_input(simd_u8 in) {
+    if (likely(in.any_bits_set(0x80u))) {
       this->check_carried_continuations();
     } else {
       this->check_utf8_bytes(in);
     }
   }
 
-  really_inline void check_next_input(simd_input in) {
-    __m256i high_bit = _mm256_set1_epi8(0x80u);
-    __m256i any_bits_on = in.reduce([&](auto a, auto b) {
-      return _mm256_or_si256(a, b);
-    });
-    if (likely(_mm256_testz_si256(any_bits_on, high_bit) == 1)) {
+  really_inline void check_next_input(simd_u8x64 in) {
+    simd_u8 bits = in.reduce([&](auto a, auto b) { return a | b; });
+    if (likely(bits.any_bits_set(0x80u))) {
       // it is ascii, we just check carried continuations.
       this->check_carried_continuations();
     } else {
@@ -221,7 +191,7 @@ struct utf8_checker {
   }
 
   really_inline ErrorValues errors() {
-    return _mm256_testz_si256(this->has_error, this->has_error) == 0 ? simdjson::UTF8_ERROR : simdjson::SUCCESS;
+    return this->has_error.any_bits_set() ? simdjson::UTF8_ERROR : simdjson::SUCCESS;
   }
 }; // struct utf8_checker
 
